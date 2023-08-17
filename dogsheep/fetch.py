9960eae3b16f892a78584e70aceb8216eb72cc97
Bartek Ciszkowski <hello@justbartek.ca>
2023-08-17 15:58:55 -0300

    Sync (St. Mary's University)

--- dogsheep/fetch.py
+++ dogsheep/fetch.py
@@ -68,7 +68,7 @@ def query_database(db_file):
         cursor = conn.cursor()
 
         # Perform the query
-        query = """SELECT * FROM apple_photos WHERE albums = '["Bike Posts Aug 14 2023"]'"""
+        query = """SELECT * FROM apple_photos WHERE albums = '["bike posts southend aug 16"]'"""
         cursor.execute(query)
 
         features = []
