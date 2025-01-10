0adb133d3f91e8f5380af227d1f1acb7a429f851
Bartek Ciszkowski <hello@justbartek.ca>
2025-01-09 21:16:38 -0400

    Add base.geojson
    This is an export from Felt, in the maps initial work. Not all these
    points are in the current source of truth (Photos), so will be merged
    from now on.

--- merge.sh
+++ merge.sh
@@ -4,4 +4,4 @@ set -e
 
 # Take partial and merge it with existing data.geojson, creating a merged.geojson
 # Once validated, can move into data.geojson
-jq -s '{type: "FeatureCollection", features: (.[0].features + .[1].features)}' data.geojson partial.geojson > merged.geojson
+jq -s '{type: "FeatureCollection", features: (.[0].features + .[1].features)}' base.geojson partial.geojson > data.geojson
