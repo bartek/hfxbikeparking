19a5ed4de53dd6702ba64357002c8ef9923cb950
Bartek Ciszkowski <hello@justbartek.ca>
2026-08-01 19:38:30 -0300

    refine sync

--- sync.sh
+++ sync.sh
@@ -1,7 +1,15 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
 set -ex
 
+cd "$(dirname "$0")"
+
+(cd photos && source .venv/bin/activate && python sync_images.py)
+
 source .venv/bin/activate
-dogsheeep-photos apple-photos photos.db
+dogsheep-photos apple-photos photos.db
 ALBUM="hfxbikeparking" python dogsheep/fetch.py > partial.geojson
 ./merge.sh
+
+git add data.geojson
+git commit -m "Sync"
+git push
